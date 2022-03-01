586a10ded9a6f72a456a5a420e2acb97ef17673a
Vladislav <vzv@siluetstudio.com>
2022-03-01 17:11:26 +0300

    mainpage

--- js/jquery-ready.js
+++ js/jquery-ready.js
@@ -48,7 +48,53 @@ $(document).ready(function() {
     //============Мобильное меню (КОНЕЦ)
 
     //Слайдер на главной
-    $('.intro .slider').slick({
-        
-    })
+    if ($('.intro .slider').length) {
+        $('.intro .slider').slick({
+
+        })
+    }    
+
+    //Слайдер партнеров
+    if ($('.partners .slider').length) {
+        $('.partners .slider').slick({
+            slidesToShow: 6,
+            dots: false,
+            prevArrow: '<button id="prev" type="button" class="slider__prev"><svg><use xlink:href="img/icons/sprite.svg#chevron"></use></svg></button>',
+            nextArrow: '<button id="next" type="button" class="slider__next"><svg><use xlink:href="img/icons/sprite.svg#chevron"></use></svg></button>'
+        });   
+    }
+
+    //слайдер помещений на главной
+    if ($('.rent-list.mainpage .slider').length) {
+        $('.rent-list.mainpage .slider').slick({
+            slidesToShow: 4,
+            dots: false,
+            prevArrow: '<button id="prev" type="button" class="slider__prev"><svg><use xlink:href="img/icons/sprite.svg#chevron"></use></svg></button>',
+            nextArrow: '<button id="next" type="button" class="slider__next"><svg><use xlink:href="img/icons/sprite.svg#chevron"></use></svg></button>'
+        });   
+    }
+
+    //карта
+    if ($('#map').length) {
+        ymaps.ready(init);
+        function init(){
+           // Создание карты.
+           var myMap = new ymaps.Map("map", {                
+                center: [54.71978356995788,20.50384850000001],         
+                zoom: 16,
+                controls: []
+            });
+
+            var myPlacemark = new ymaps.Placemark([54.71978356995788,20.50384850000001], {}, {
+                iconLayout: 'default#image',
+                iconImageHref: 'img/icons/map-marker.png',
+                iconImageSize: [55, 57],
+                iconImageOffset: [-28, -45]
+            });
+
+            myMap.geoObjects.add(myPlacemark);
+            myMap.behaviors.disable('scrollZoom')
+        }
+    }
+    
 });
