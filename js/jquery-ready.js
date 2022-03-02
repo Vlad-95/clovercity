f153296de1df7f3a0753edacd8586a24f3428d1a
Vladislav <vzv@siluetstudio.com>
2022-03-02 17:15:06 +0300

    about page

--- js/jquery-ready.js
+++ js/jquery-ready.js
@@ -96,5 +96,15 @@ $(document).ready(function() {
             myMap.behaviors.disable('scrollZoom')
         }
     }
+
+    //О компании - слайдер
+    if ($('.about .slider').length) {
+        $('.about .slider').slick({
+            slidesToShow: 3,
+            dots: false,
+            prevArrow: '<button id="prev" type="button" class="slider__prev"><svg><use xlink:href="img/icons/sprite.svg#chevron"></use></svg></button>',
+            nextArrow: '<button id="next" type="button" class="slider__next"><svg><use xlink:href="img/icons/sprite.svg#chevron"></use></svg></button>'
+        });   
+    }
     
 });
